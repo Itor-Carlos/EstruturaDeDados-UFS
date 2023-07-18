4c0dbeb0697eae7931b2fe78c47c6f0b9537c024
Itor Carlos Souza Queiroz <72228871+Itor-Carlos@users.noreply.github.com>
2023-07-17 22:44:29 -0300

    definido metodo de inserção de dados na estrutura LISTA ENCADEADA

--- Lista Encadeada/lista_encadeada.c
+++ Lista Encadeada/lista_encadeada.c
@@ -9,3 +9,13 @@ typedef struct {
     NO* cabeca;
     int tamanho;
 } LISTA_ENCADEADA;
+
+void inserir(LISTA_ENCADEADA *lista, int item){
+    lista->cabeca = criarNo(item, lista->cabeca);
+    lista->tamanho++;
+}
+
+void inicializar(LISTA_ENCADEADA *lista){
+    lista->cabeca = NULL;
+    lista->tamanho = 0;
+}
